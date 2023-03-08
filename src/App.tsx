6e6704f46b2ddb31839083e51b73b9476f242bc4
Gabriel Frasson <bielfc27@gmail.com>
2023-03-08 18:37:12 -0300

    Start project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from "styled-components";
+
+import { Button } from "./components/Button";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export { App };
