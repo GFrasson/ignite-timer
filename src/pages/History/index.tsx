360ff1e0cc86b6546a758af7a4996bc22f45da46
Gabriel Frasson <bielfc27@gmail.com>
2023-04-04 18:14:56 -0300

    Fix: bugs of date type cast on history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -22,35 +22,31 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            {
-              cycles.map(cycle => {
-                return (
-                  <tr key={cycle.id}>
-                    <td>{cycle.task}</td>
-                    <td>{cycle.minutesAmount} minutos</td>
-                    <td>
-                      {formatDistanceToNow(cycle.startDate, {
-                        addSuffix: true,
-                        locale: ptBR
-                      })}
-                    </td>
-                    <td>
-                      {
-                        cycle.finishedDate && <Status statusColor="green">Concluído</Status>
-                      }
-                      {
-                        cycle.interruptedDate && <Status statusColor="red">Interrompido</Status>
-                      }
-                      {
-                        (!cycle.finishedDate && !cycle.interruptedDate) && (
-                          <Status statusColor="yellow">Em andamento</Status>
-                        )
-                      }
-                    </td>
-                  </tr>
-                )
-              })
-            }
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor="yellow">Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
